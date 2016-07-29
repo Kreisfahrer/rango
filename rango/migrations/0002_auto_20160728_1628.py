a2931a16bdcb851435f96f5fed692bafa8d36e39
kreisfahrer <andreystakhievich@coherentsolutions.com>
2016-07-29 19:42:03 +0300

    Added new properties for category

---
+++ rango/migrations/0002_auto_20160728_1628.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='category',
+            options={'verbose_name': 'Category', 'verbose_name_plural': 'Categories'},
+        ),
+        migrations.AlterModelOptions(
+            name='page',
+            options={'verbose_name': 'Page', 'verbose_name_plural': 'Pages'},
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='likes',
+            field=models.IntegerField(default=0),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='views',
+            field=models.IntegerField(default=0),
+            preserve_default=True,
+        ),
+    ]
